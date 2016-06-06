83eb76864add37b8bb87c65352124919b428e320
Marc <marczoeller@gmx.net>
2016-06-06 14:12:48 +0200

    Highlight area between curves

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -16,13 +16,24 @@ def showAllDistributions(sequence):
                 showDistributions(calculatedRule.distribution, rule.distribution)
 
 
-def showDistributions(estimatedDist, trueDist):
+def showDistributions(estimatedDist, trueDist, visualizeOverlap=True):
     borders1 = estimatedDist.dist.interval(0.99)
     borders2 = trueDist.dist.interval(0.99)
-    x = np.linspace(min(borders1[0], borders2[0]), max(borders1[1], borders2[1]), 500)
-    plt.plot(x, estimatedDist.dist.pdf(x), "b", label="Estimated distribution")
-    plt.plot(x, trueDist.dist.pdf(x), "r", label="True distribution")
+    x = np.linspace(min(borders1[0], borders2[0]) - 1, max(borders1[1], borders2[1]) + 1, 500)
+    y1 = estimatedDist.dist.pdf(x)
+    y2 = trueDist.dist.pdf(x)
+
+    plt.plot(x, y1, "b", label="Estimated distribution")
+    plt.plot(x, y2, "r", label="True distribution")
     plt.legend()
+
+    if (visualizeOverlap):
+        ax = plt.gca()
+        y = np.zeros(len(y1))
+        for i in range(len(y1)):
+            y[i] = min(y1[i], y2[i])
+        ax.fill_between(x, 0, y, facecolor="#E8E8E8", edgecolor="#E8E8E8")
+
     plt.show()
 
 
