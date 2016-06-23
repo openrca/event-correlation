e521b15de859283c60e8ecc5336ce04621422610
Marc <marczoeller@gmx.net>
2016-06-23 14:32:11 +0200

    Rework IcpMatcher for 1d data.
    So far, the IcpMatcher handled two dimensional data. This is not necessary
    as all data are 1d. Rework the algorithm for better runtime.

--- algorithms/icpMatcher.py
+++ algorithms/icpMatcher.py
@@ -3,7 +3,6 @@ from time import sleep
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
-from sklearn.neighbors import NearestNeighbors
 
 from algorithms import Matcher, RESULT_MU, RESULT_SIGMA, RESULT_KDE
 from core.distribution import KdeDistribution
@@ -16,6 +15,7 @@ class IcpMatcher(Matcher):
         self.maxiter = 50
         self.threshold = 1e-4
         self.showVisualization = False
+        self.switched = False
 
     def parseArgs(self, kwargs):
         """
@@ -31,147 +31,110 @@ class IcpMatcher(Matcher):
         if ("threshold" in kwargs):
             self.threshold = kwargs["threshold"]
         if ("initPos" in kwargs):
-            self.initPose = np.array(kwargs["initPos"]).astype(np.float32)
+            self.initPose = kwargs["initPos"]
         if ("showVisualization" in kwargs):
             self.showVisualization = kwargs["showVisualization"]
 
     def compute(self, src=None, dst=None):
         if (src is None):
-            src = np.array(self.sequence.asVector(self.eventA), copy=True).astype(np.float32)
-            # artificially shift data for better visualization
-            src = np.column_stack((src, np.ones(src.size) * -0.5))
-
+            src = np.array(self.sequence.asVector(self.eventA))
         if (dst is None):
-            dst = np.array(self.sequence.asVector(self.eventB), copy=True).astype(np.float32)
-            dst = np.column_stack((dst, np.zeros(dst.size)))
+            dst = np.array(self.sequence.asVector(self.eventB))
 
-        a = np.array(src, copy=True).astype(np.float32)
-        b = np.array(dst, copy=True).astype(np.float32)
+        self.switched = len(src) > len(dst)
+        if (self.switched):
+            self.logger.info("Switching src and dst")
+            tmp = src
+            src = dst
+            dst = tmp
+
+        a = np.array(src, copy=True).astype(float)
+        b = np.array(dst, copy=True).astype(float)
 
         if (self.initPose is None):
             self.initPose = self.findInitialGuess(a, b)
 
         opt = np.array(self.initPose).astype(np.float32)
-        a = IcpMatcher.transform(opt, a)
+        a += opt
 
         p = None
         for i in range(self.maxiter):
-            if (p is not None and np.linalg.norm(p) < self.threshold):
+            if (p is not None and abs(p) < self.threshold):
                 break
 
             idx = IcpMatcher.findMinimalDistance(a, b)
             p = IcpMatcher.findOptimalTransformation(opt, a, b[idx])
-            a = IcpMatcher.transform(p, a)
+            a += p
             opt += p
 
-            self.logger.debug("Offset {}\t Distance {}".format(opt, IcpMatcher.totalDistance(np.array([0, 0]),
-                                                                                             a, b[idx])))
+            self.logger.debug("Offset {}\t Distance {}".format(opt, IcpMatcher.totalDistance(0, a, b[idx])))
             if (self.showVisualization):
                 IcpMatcher.visualizeCurrentStep(src, dst, a, b[idx])
 
         idx = IcpMatcher.findMinimalDistance(a, b)
-        self.logger.info("Final distance " + str(IcpMatcher.totalDistance(np.array([0, 0]), a, b[idx])))
-        self.logger.info("Final offset " + str(opt))
-
-        idx = IcpMatcher.findMinimalDistance(a, b)
-        print(idx)
         tmp = b[idx]
+        self.logger.info("Final distance " + str(IcpMatcher.totalDistance(0, a, tmp)))
+        self.logger.info("Final offset " + str(opt))
+        print(idx)
 
-        cost = np.zeros(np.shape(a))
-        cost[:, 0] = tmp[:, 0] - src[:, 0]
-        # cost[:, 1] = tmp[:, 1] - src[:, 1]
-        cost = np.linalg.norm(cost, axis=1)
-
+        cost = tmp - src
         cost = cost[abs(cost - cost.mean()) < 2.58 * cost.std()]
-        print(cost)
+        if (self.switched):
+            cost *= -1
 
         return {RESULT_MU: cost.mean(), RESULT_SIGMA: cost.std(), RESULT_KDE: KdeDistribution(cost), "Offset": opt}
 
     def findInitialGuess(self, a, b):
-        [Ax, Bx] = np.meshgrid(a[:, 0], b[:, 0])
-        guessX = (Bx - Ax).mean()
-
-        [Ay, By] = np.meshgrid(a[:, 1], b[:, 1])
-        guessY = (By - Ay).mean()
-
-        guess = np.array([guessX, guessY]).astype(np.float32)
+        [A, B] = np.meshgrid(a, b)
+        guess = (B - A).mean()
         self.logger.info("Estimated initial guess as {}".format(guess))
         return guess
 
     @staticmethod
     def visualizeCurrentStep(src, dst, a, b):
         plt.clf()
-        plt.scatter(dst[:, 0], dst[:, 1], marker='x', c='b', label="Reference")
-        plt.scatter(src[:, 0], src[:, 1], marker='x', c='r', label="Input")
+        plt.scatter(dst, [0] * len(dst), marker='x', c='b', label="Reference")
+        plt.scatter(src, [-1] * len(src), marker='x', c='r', label="Input")
 
-        plt.scatter(a[:, 0], a[:, 1], marker='x', c='g', label="Shifted Result")
+        plt.scatter(a, [-0.5] * len(a), marker='x', c='g', label="Shifted Result")
         plt.legend(loc="upper left")
-        # plt.xlim([0, 200])
-        # plt.ylim([-2, 2])
+        plt.ylim([-1.2, 0.4])
 
         for i in range(a.shape[0]):
-            plt.plot([src[i, 0], b[i, 0]], [src[i, 1], b[i, 1]], color='k', linestyle='-', linewidth=0.5)
+            plt.plot([src[i], b[i]], [-1, 0], color='k', linestyle='-', linewidth=0.5)
+            plt.plot([a[i], b[i]], [-0.5, 0], color='k', linestyle=':', linewidth=0.5)
 
         plt.show()
         sleep(1)
 
     @staticmethod
     def findMinimalDistance(a, b):
-        neighbours = NearestNeighbors(n_neighbors=1, algorithm='auto').fit(b)
-        distances, idx = neighbours.kneighbors(a)
-        return idx.flatten()
+        [A, B] = np.meshgrid(a, b)
+        delta = abs(B - A)
+        return delta.argmin(axis=0)
 
     @staticmethod
     def findOptimalTransformation(p, a, b):
         # Compute minimum of cost function
-        #   sum( (T * a - b) * (T * a - b)' )
-        # with
-        #   T = [[ 1 0 p_x],
-        #        [ 0 1 p_y]]
-        # and p the variable.
+        #   sum( (a + p - b)^2 )
+        # with p the variable.
         result = optimize.minimize(IcpMatcher.totalDistance, p, args=(a, b), method='Newton-CG',
                                    jac=IcpMatcher.jacobiMatrix, hess=IcpMatcher.hesseMatrix)
-        return result.x
+        return result.x[0]
 
     @staticmethod
     def totalDistance(p, a, b):
-        xt = IcpMatcher.transform(p, a)
-
-        d = np.zeros(np.shape(a))
-        d[0, :] = xt[0, :] - b[0, :]
-        d[1, :] = xt[1, :] - b[1, :]
-
-        return np.sum(np.square(d[:, 0]) + np.square(d[:, 1]))
-
-    @staticmethod
-    def transform(p, points):
-        T = p
-        if (p.ndim == 1):
-            T = np.array([[1, 0, p[0]],
-                          [0, 1, p[1]]])
-
-        tmp = np.column_stack((points, np.ones(len(points))))
-        dst = np.zeros(points.shape)
-
-        for i in range(len(points)):
-            dst[i] = T.dot(tmp[i])
-        return dst
+        return np.sum(np.square((a + p) - b))
 
     # noinspection PyTypeChecker
     @staticmethod
     def jacobiMatrix(p, a, b):
-        xt = IcpMatcher.transform(p, a)
-
-        d = np.zeros(np.shape(a))
-        d[:, 0] = xt[:, 0] - b[:, 0]
-        d[:, 1] = xt[:, 1] - b[:, 1]
-
-        return np.array([np.sum(2 * d[:, 0]),
-                         np.sum(2 * d[:, 1])])
+        d = (a + p) - b
+        return np.array([np.sum(2 * d)])
 
     # noinspection PyUnusedLocal
     @staticmethod
     def hesseMatrix(p, a, b):
         # This function has to have the same arguments as IcpMatcher.jacobiMatrix and IcpMatcher.totalDistance.
         # See http://docs.scipy.org/doc/scipy-0.17.1/reference/generated/scipy.optimize.minimize.html
-        return np.eye(2) * 2 * np.size(a, 0)
+        return 2 * np.size(a, 0)
--- test/algorithm/icpMatcher.py
+++ test/algorithm/icpMatcher.py
@@ -2,18 +2,15 @@ import unittest
 
 import numpy as np
 
+from algorithms import RESULT_MU
 from algorithms.icpMatcher import IcpMatcher
 
 
 class TestScript(unittest.TestCase):
-    def test_transform(self):
-        points = np.array([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]])
-        p = np.array([-0.5, 0.5])
-        res = IcpMatcher.transform(p, points)
-        self.assertTrue(
-            np.array_equal(np.array([[-0.5, 0.5], [0.5, 1.5], [1.5, 2.5], [2.5, 3.5], [3.5, 4.5], [4.5, 5.5]]), res))
+    def test_square(self):
+        a = np.array([5, 20, 27])
+        b = np.array([12, 25, 32])
 
-        res = IcpMatcher.transform(np.array([[1, 0, p[0]],
-                                             [0, 1, p[1]]]), points)
-        self.assertTrue(
-            np.array_equal(np.array([[-0.5, 0.5], [0.5, 1.5], [1.5, 2.5], [2.5, 3.5], [3.5, 4.5], [4.5, 5.5]]), res))
+        matcher = IcpMatcher()
+        res = matcher.compute(a, b)
+        self.assertAlmostEqual(res[RESULT_MU], 17 / 3)
