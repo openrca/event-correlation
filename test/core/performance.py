3d2dd2166a1099ae6cf83149aad7cc65ab73dd5b
Marc <marczoeller@gmx.net>
2016-08-30 12:43:45 +0200

    Fix test case

--- test/core/performance.py
+++ test/core/performance.py
@@ -6,8 +6,8 @@ from core.performance import PearsonCoefficient, DistanceCorrelation, EnergyStat
 
 
 class TestScript(unittest.TestCase):
-    list1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
-    list2 = np.array([-1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14, -15, 16, -17, 18, -19, 200])
+    list1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+    list2 = np.array([-8, -72, 80, -9, -8, 25, -54, 65, 52, -1, -72, 66, 91, -65, -73])
 
     def testPearsonCoefficient(self):
         a = np.array(
