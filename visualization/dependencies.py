091f58a4059d90008d2af29016b3ba730e7658b5
Marc <marczoeller@gmx.net>
2016-12-06 14:00:27 +0100

    Add tree trimming based on mutual information

--- visualization/dependencies.py
+++ visualization/dependencies.py
@@ -2,8 +2,8 @@ import math
 
 import networkx as nx
 import numpy as np
-from PySide.QtCore import QPoint, Signal
-from PySide.QtGui import QGraphicsScene, QGraphicsView, QHBoxLayout, QWidget, QSizePolicy
+from PySide.QtCore import QPoint, Signal, Qt
+from PySide.QtGui import QGraphicsScene, QGraphicsView, QHBoxLayout, QWidget, QSizePolicy, QVBoxLayout, QSlider
 
 from core.event import Event
 from core.rule import Rule
@@ -12,8 +12,8 @@ from visualization.details import DetailsContainer
 
 
 class ResponsiveArrowWidget(ArrowWidget):
-    def __init__(self, start, end, parent=None):
-        super().__init__(start, end)
+    def __init__(self, start, end, color=0, parent=None):
+        super().__init__(start, end, color=color)
         self._callbackParam = None
         self.__parent = parent
 
@@ -23,20 +23,38 @@ class ResponsiveArrowWidget(ArrowWidget):
 
 
 class DependenciesView(QWidget):
+    __paintSeq = Signal()
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Dependency Graph')
+        self.__mutualInformation = np.zeros(1)
 
         layout = QHBoxLayout()
         self.setLayout(layout)
 
+        layoutLeft = QVBoxLayout()
         self.__tree = DependencyTree(self)
+        self.__paintSeq.connect(self.__tree.paint)
         self.__view = QGraphicsView()
         self.__view.setScene(self.__tree)
+        layoutLeft.addWidget(self.__view)
+
+        self.__slider = QSlider(Qt.Orientation.Horizontal)
+        self.__slider.setRange(0, 100)
+        self.__slider.setSliderPosition(100)
+        self.__slider.setTickInterval(25)
+        self.__slider.setTickPosition(QSlider.TickPosition.TicksBelow)
+        # noinspection PyUnresolvedReferences
+        self.__slider.valueChanged.connect(self.__sensitivityChanged)
+        layoutLeft.addWidget(self.__slider)
+
+        containerLeft = QWidget()
         policyLeft = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         policyLeft.setHorizontalStretch(2)
-        self.__view.setSizePolicy(policyLeft)
-        layout.addWidget(self.__view)
+        containerLeft.setSizePolicy(policyLeft)
+        containerLeft.setLayout(layoutLeft)
+        layout.addWidget(containerLeft)
 
         self.__details = DetailsContainer(showPlot=True, showAssignments=False)
         policyRight = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
@@ -45,6 +63,10 @@ class DependenciesView(QWidget):
         layout.addWidget(self.__details, 0, 2)
 
     def setData(self, root, sequence):
+        self.__mutualInformation = np.zeros(len(sequence.calculatedRules))
+        for index, rule in enumerate(sequence.calculatedRules):
+            self.__mutualInformation[index] = rule.data["Mutual Information"]
+        self.__sensitivityChanged(redraw=False)
         self.__tree.setData(root, sequence)
 
     def setDetails(self, sequence, rule):
@@ -60,6 +82,17 @@ class DependenciesView(QWidget):
         self.__view.items().clear()
         self.__tree.paint()
 
+    def __sensitivityChanged(self, redraw=True):
+        epsilon = self.__slider.value() / 100
+        percentile25 = np.percentile(self.__mutualInformation, 25)
+        percentile75 = np.percentile(self.__mutualInformation, 75)
+
+        threshold = percentile25 - (0.5 + epsilon) * (percentile75 - percentile25) + 5
+        self.__tree._threshold = threshold
+        if (redraw):
+            self.__view.items().clear()
+            self.__paintSeq.emit()
+
 
 class DependencyTree(QGraphicsScene):
     detailsSignal = Signal(Rule)
@@ -73,6 +106,7 @@ class DependencyTree(QGraphicsScene):
         self.__sequence = None
         self.__graph = None
         self.__positions = None
+        self._threshold = 0
         self.detailsSignal.connect(self.__showDetails)
 
     def setData(self, root, sequence):
@@ -142,7 +176,6 @@ class DependencyTree(QGraphicsScene):
         size = [self.__parent.width() * 0.5, self.__parent.height() * 0.8]
 
         for start, end in self.__graph.edges():
-            # TODO problem if start -> end and end -> start
             startPoint = self.__pointToPlane(self.__positions[start], size, center=True)
             endPoint = self.__clipPointToCircle(startPoint,
                                                 self.__pointToPlane(self.__positions[end], size, center=True))
@@ -151,7 +184,8 @@ class DependencyTree(QGraphicsScene):
             if (rule is None):
                 rule = self.__sequence.getCalculatedRule(end, start)
             if (rule is not None):
-                widget = ResponsiveArrowWidget(startPoint, endPoint, parent=self)
+                color = 200 if (rule.data["Mutual Information"] < self._threshold) else 0
+                widget = ResponsiveArrowWidget(startPoint, endPoint, color=color, parent=self)
                 widget._callbackParam = self.__sequence.getCalculatedRule(start, end)
                 self.addItem(widget)
 
