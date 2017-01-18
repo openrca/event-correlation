6533d12486875b5ac3477fce2415da3faa97e59e
Marc <marczoeller@gmx.net>
2017-01-18 16:24:04 +0100

    Finish synthetic sequences

--- scripts/syntheticSquences.py
+++ scripts/syntheticSquences.py
@@ -11,13 +11,16 @@ from algorithms.icpMatcher import IcpMatcher
 from algorithms.lagEM import lagEM
 from algorithms.lpMatcher import LpMatcher, Method
 from core import distribution
+from core.timer import Timer
 from provider.generator import Generator
 from visualization import CORRECT, ICP, LP, LAGEM, plotDistributions
 
-inputFiles = ['../contrib/scenarios/2.json']
-sequenceCount = 3
+inputFile = '../contrib/scenarios/3.json'
+sequenceCount = 1
 create = True
-plot = True
+plot = False
+
+outputFile = os.path.toAbsolutePath(os.path.splitext(inputFile)[0] + '-out.json')
 
 
 class Result:
@@ -51,88 +54,91 @@ class Result:
         return self.trigger == other.trigger and self.response == other.response
 
 
+timer = Timer()
+timer.start()
 logger = logging.getLogger()
-for file in inputFiles:
-    if (create):
-        logger.info('Processing ' + file)
-        resultIcp = {}
-        resultLp = {}
-        resultLagEm = {}
-        keys = set()
+if (create):
+    logger.info('Processing ' + inputFile)
+    resultIcp = {}
+    resultLp = {}
+    resultLagEm = {}
+    keys = set()
 
-        sequence = None
-        for i in range(sequenceCount):
-            logger.debug('\tRound ' + str(i))
-            sequence = Generator().create(file)
+    sequence = None
+    for i in range(sequenceCount):
+        logger.debug('\tRound ' + str(i))
+        sequence = Generator().create(inputFile)
 
-            listIcp = IcpMatcher().matchAll(sequence, f="confidence")
-            for rule in listIcp:
-                key = (rule.trigger, rule.response)
-                keys.add(key)
-                resultIcp.setdefault(key, Result(key[0], key[1])).addData(rule.data)
+        listIcp = IcpMatcher().matchAll(sequence, f="confidence")
+        for rule in listIcp:
+            key = (rule.trigger, rule.response)
+            keys.add(key)
+            resultIcp.setdefault(key, Result(key[0], key[1])).addData(rule.data)
 
-            listLp = LpMatcher().matchAll(sequence, algorithm=Method.PULP)
-            for rule in listLp:
-                key = (rule.trigger, rule.response)
-                keys.add(key)
-                resultLp.setdefault(key, Result(key[0], key[1])).addData(rule.data)
+        listLp = LpMatcher().matchAll(sequence, algorithm=Method.PULP)
+        for rule in listLp:
+            key = (rule.trigger, rule.response)
+            keys.add(key)
+            resultLp.setdefault(key, Result(key[0], key[1])).addData(rule.data)
 
-            listLagEM = lagEM().matchAll(sequence, threshold=0.01, enforceNormal=True)
-            for rule in listLagEM:
-                key = (rule.trigger, rule.response)
-                keys.add(key)
-                resultLagEm.setdefault(key, Result(key[0], key[1])).addData(rule.data)
+        listLagEM = lagEM().matchAll(sequence, threshold=0.01, enforceNormal=True)
+        for rule in listLagEM:
+            key = (rule.trigger, rule.response)
+            keys.add(key)
+            resultLagEm.setdefault(key, Result(key[0], key[1])).addData(rule.data)
 
-        with open(os.path.splitext(file)[0] + '-out.json', 'w') as out:
-            data = []
-            gen = Generator()
-            gen.create(file)
-            for key in keys:
-                entry = {
-                    'trigger': key[0],
-                    'response': key[1],
-                    'count': gen.getNumberOfEvents()
-                }
-                data.append(entry)
+    data = []
+    gen = Generator()
+    gen.create(inputFile)
+    for key in keys:
+        entry = {
+            'trigger': key[0],
+            'response': key[1],
+            'count': gen.getNumberOfEvents()
+        }
+        data.append(entry)
 
-                correct = sequence.getBaseDistribution(key[0], key[1])
-                if (correct is not None):
-                    entry[CORRECT] = correct
+        correct = sequence.getBaseDistribution(key[0], key[1])
+        if (correct is not None):
+            entry[CORRECT] = correct
 
-                icp = resultIcp.get(key)
-                if (icp is not None):
-                    entry[ICP] = {
-                        'mean': icp.getMean(),
-                        'sigma': icp.getSigma(),
-                        'samples': icp.getSamples()
-                    }
+        icp = resultIcp.get(key)
+        if (icp is not None):
+            entry[ICP] = {
+                'mean': icp.getMean(),
+                'sigma': icp.getSigma(),
+                'samples': icp.getSamples()
+            }
 
-                lp = resultLp.get(key)
-                if (lp is not None):
-                    entry[LP] = {
-                        'mean': lp.getMean(),
-                        'sigma': lp.getSigma(),
-                        'samples': lp.getSamples()
-                    }
+        lp = resultLp.get(key)
+        if (lp is not None):
+            entry[LP] = {
+                'mean': lp.getMean(),
+                'sigma': lp.getSigma(),
+                'samples': lp.getSamples()
+            }
 
-                lagEM = resultLagEm.get(key)
-                if (lagEM is not None):
-                    entry[LAGEM] = {
-                        'mean': lagEM.getMean(),
-                        'sigma': lagEM.getSigma(),
-                        'samples': lagEM.getSamples()
-                    }
-            json.dump(data, out, default=core.defaultJsonEncoding)
+        lagEM = resultLagEm.get(key)
+        if (lagEM is not None):
+            entry[LAGEM] = {
+                'mean': lagEM.getMean(),
+                'sigma': lagEM.getSigma(),
+                'samples': lagEM.getSamples()
+            }
+    with open(outputFile, 'w') as out:
+        json.dump(data, out, default=core.defaultJsonEncoding)
+timer.stop()
+logger.info("Calculation finished after {}".format(timer))
 
-    if (plot):
-        with open(os.path.toAbsolutePath(os.path.splitext(file)[0] + '-out.json'), 'r') as input:
-            content = input.readlines()
-            data = json.loads("".join(content))
-            for entry in data:
-                plotDistributions({
-                    CORRECT: distribution.load(entry[CORRECT]) if CORRECT in entry else None,
-                    ICP: entry[ICP]['samples'] if ICP in entry else None,
-                    LP: entry[LP]['samples'] if LP in entry else None,
-                    LAGEM: [entry[LAGEM]['mean'], entry[LAGEM]['sigma']] if LAGEM in entry else None
-                }, '{}-{}'.format(entry['trigger'], entry['response']))
-            plt.show()
+if (plot):
+    with open(outputFile, 'r') as input:
+        content = input.readlines()
+        data = json.loads("".join(content))
+        for entry in data:
+            plotDistributions({
+                CORRECT: distribution.load(entry[CORRECT]) if CORRECT in entry else None,
+                ICP: entry[ICP]['samples'] if ICP in entry else None,
+                LP: entry[LP]['samples'] if LP in entry else None,
+                LAGEM: [entry[LAGEM]['mean'], entry[LAGEM]['sigma']] if LAGEM in entry else None
+            }, '{}-{}'.format(entry['trigger'], entry['response']))
+        plt.show()
